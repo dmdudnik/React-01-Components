d58245ab91c95a32f1bc98f5b7cfc4f0f962d712
Dmitriy Dudnik <ddudnik79@gmail.com>
2022-02-08 23:03:08 +0200

    add render quantity

--- src/App.js
+++ src/App.js
@@ -6,29 +6,35 @@ const painting = paintings;
 
 
 export default function App() {
+  const isOnline = true;
   return (
     <div>
+      {isOnline && 'Онлайн'}
+      { isOnline? 'Online' :'Offline'}
       <Painting
-    imgUrl={painting[0].url}
-    title={painting[0].title}
-    authorName={painting[0].author.tag}
-    profileUrl={painting[0].author.url}
-    price={painting[0].price}
-  />,
+        imgUrl={painting[0].url}
+        title={painting[0].title}
+        authorName={painting[0].author.tag}
+        profileUrl={painting[0].author.url}
+        price={painting[0].price}
+        quantity={ painting[0].quantity}
+  />
       <Painting
-    imgUrl={painting[1].url}
-    title={painting[1].title}
-    authorName={painting[1].author.tag}
-    profileUrl={painting[1].author.url}
-    price={painting[1].price}
-      />,
+        imgUrl={painting[1].url}
+        title={painting[1].title}
+        authorName={painting[1].author.tag}
+        profileUrl={painting[1].author.url}
+        price={painting[1].price}
+        quantity={ painting[1].quantity}
+      />
       <Painting
-    imgUrl={painting[2].url}
-    title={painting[2].title}
-    authorName={painting[2].author.tag}
-    profileUrl={painting[2].author.url}
-    price={painting[2].price}
-  />,
+      imgUrl={painting[2].url}
+      title={painting[2].title}
+      authorName={painting[2].author.tag}
+      profileUrl={painting[2].author.url}
+      price={painting[2].price}
+      quantity={ painting[2].quantity}
+  />
     </div>
     
   );
--- src/components/Painting.js
+++ src/components/Painting.js
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types'
 import defaultImage from '../components/default.jpg'
 
-const Painting = ({ imgUrl = defaultImage, title, profileUrl, authorName = "неизвестно", price }) => {
+const Painting = ({ imgUrl = defaultImage, title, profileUrl, authorName = "неизвестно", price, quantity }) => {
   return <div>
     <img src={imgUrl} alt= {title} width="480" />
     <h2>{title}</h2>
     <p>Автор: <a href={profileUrl}>{authorName}</a></p>
     <p>Цена: {price} кредитов</p>
-    <p>Доступность: заканчивается или есть в наличии}</p>
+    <p>Доступность: {quantity < 10 ? 'заканчивается': 'есть в наличии'}</p>
     <button type="button">Добавить в корзину</button>
   </div>
   
@@ -20,6 +20,7 @@ Painting.propTypes = {
     profileUrl: PropTypes.string,
     authorName: PropTypes.string,
     price: PropTypes.number,
+    quantity: PropTypes.number,
 }
 
 
