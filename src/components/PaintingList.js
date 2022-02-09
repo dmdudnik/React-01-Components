4d5418f877cce9a6a76c27d4f694c84c932ca286
Dmitriy Dudnik <ddudnik79@gmail.com>
2022-02-09 23:17:04 +0200

    add collection PaintingList

---
+++ src/components/PaintingList.js
@@ -0,0 +1,23 @@
+import Painting from './Painting';
+function PaintingList({ items }) {
+    return(
+    <ul>
+        {items.map(item => (
+            <li key={item.id}>
+                <Painting
+        imgUrl={item.url}
+        title={item.title}
+        authorName={item.author.tag}
+        profileUrl={item.author.url}
+        price={item.price}
+        quantity={item.quantity}
+      />
+            </li>
+        ))}
+        </ul>
+    )
+
+    
+}
+
+export default PaintingList;
