a12cc9f815f52b925ed13c42b5328282ee19846b
Dmitriy Dudnik <ddudnik79@gmail.com>
2022-02-09 23:22:58 +0200

    update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <div>
       <PaintingList items={paintings}/>
-    </div>
-    
+    </div> 
   );
 }
--- src/components/PaintingList.js
+++ src/components/PaintingList.js
@@ -1,23 +1,21 @@
 import Painting from './Painting';
 function PaintingList({ items }) {
-    return(
-    <ul>
-        {items.map(item => (
-            <li key={item.id}>
-                <Painting
-        imgUrl={item.url}
-        title={item.title}
-        authorName={item.author.tag}
-        profileUrl={item.author.url}
-        price={item.price}
-        quantity={item.quantity}
-      />
-            </li>
-        ))}
+    return (
+        <ul>
+            {items.map(item => (
+                <li key={item.id}>
+                    <Painting
+                        imgUrl={item.url}
+                        title={item.title}
+                        authorName={item.author.tag}
+                        profileUrl={item.author.url}
+                        price={item.price}
+                        quantity={item.quantity}
+                    /> 
+                </li>
+            ))}
         </ul>
     )
-
-    
 }
 
 export default PaintingList;
